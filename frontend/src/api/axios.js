6afab7fd69419a90fce98f5cd663effa4f24dafc
Akash Jadhav <102428182+jadhavakashofficial@users.noreply.github.com>
2025-02-02 18:43:57 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api', // Backend URL
+  baseURL: 'https://school-crm-8uq8.onrender.com', // Backend URL
   withCredentials: true, // Send cookies with requests
 });
 
