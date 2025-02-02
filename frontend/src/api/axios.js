8fb292a8c2dee0aa780e5bd029ae92a64cec6454
Akash Jadhav <102428182+jadhavakashofficial@users.noreply.github.com>
2025-02-02 19:24:08 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,12 @@
 // frontend/src/api/axios.js
-
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://school-crm-8uq8.onrender.com/api', // Backend URL
-  withCredentials: true, // Send cookies with requests
+  baseURL: "https://school-crm-8uq8.onrender.com/api", // ensure this is correct
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true, // This ensures cookies are sent with each request
 });
 
 export default axiosInstance;
