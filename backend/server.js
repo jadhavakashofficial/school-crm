3cbcdd3ed20e6a4ec3ddff89960fc15a868ec03c
Akash Jadhav <102428182+jadhavakashofficial@users.noreply.github.com>
2025-02-03 12:10:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -52,8 +52,8 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     secure: process.env.NODE_ENV === 'production', 
-    httpsOnly: true,
-    sameSite: process.env.NODE_ENV === 'none',
+    httpOnly: true,
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 1000 * 60 * 60 * 24,
   },
 }));
