b8d4b42f570c60ce2745d2af538f931a6ade97c5
cybersniperop <67035758+cybersniperop@users.noreply.github.com>
2025-02-03 10:17:41 +0530

    Revert "Update axios.js"
    This reverts commit 8fb292a8c2dee0aa780e5bd029ae92a64cec6454.

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,12 +1,10 @@
 // frontend/src/api/axios.js
-import axios from "axios";
+
+import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "https://school-crm-8uq8.onrender.com/api", // ensure this is correct
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: true, // This ensures cookies are sent with each request
+  baseURL: 'https://school-crm-8uq8.onrender.com/api', // Backend URL
+  withCredentials: true, // Send cookies with requests
 });
 
 export default axiosInstance;
